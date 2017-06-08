2d7a7b6c3bc4609df139d35c9bc65c32bdcc3a59
Marko Kollo <mrxkollo@users.noreply.github.com>
2017-06-08 20:07:18 +0300

    Prototüübi lisamine

---
+++ website/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .backend import *
+from .forms import InputForm
+
+
+def index(request):
+	
+
+	# Genereerib tavalise formi HTML's.
+	if request.method != "POST":
+		form = InputForm()
+		return render(request, "website/index.html", {'form': form})
+
+		
+	# Juhul kui form on saadetud, võta POST sees tekst ja lase läbi EstNLTK backendi.
+	# df filtreerib tekstist välja arvud (300), nimed ja lausemärgid/sümbolid.
+	# counted_lemmas kujul [['lemma', kogus], ['lemma', kogus], ['lemma', kogus], ['lemma', kogus]]
+	# get_letter_sequence võtab sisse lemmade listi ja n-grami suuruse ja tagastab need kujul [['tähejäriendid', kogus], ['tähejäriendid', kogus]].
+	else:
+		df = get_filtered_content(request.POST.get("Tekst"))
+		counted_lemmas = count_lemmas(df["lemmas"].tolist())
+		letter_sequence = get_letter_sequence(list(df.lemmas), int(request.POST.get('n_gram')))
+
+		form = InputForm()
+		return render(request, "website/index.html", {'form': form, 'lemmas': counted_lemmas, 'letters':letter_sequence})
