68907523b86624bc8308ea5c661a467b11faa7ce
Joosep Hint <jhint3@users.noreply.github.com>
2017-06-09 11:45:11 +0300

    validator

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Content
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
 
 # Register your models here.
 admin.site.register(Content)
--- website/models.py
+++ website/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
-
 # Create your models here.
 from django.utils import timezone
+
+from django.core.validators import MinValueValidator
 
 
 class Content(models.Model):
 	user = models.ForeignKey("auth.User")
 	Tekst = models.TextField(default="")
-	n_gram = models.IntegerField()
+	n_gram = models.PositiveIntegerField(validators=[MinValueValidator(1)], default=1)
 	created = models.DateTimeField(default=timezone.now)
+	
 
 	def __str__(self):
 		return self.user + "_" + self.created
+
+	
