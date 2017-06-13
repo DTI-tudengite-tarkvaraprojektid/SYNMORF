8c4554fc276b52c27dc712a5dc7760870092e4ea
Marko Kollo <mrxkollo@users.noreply.github.com>
2017-06-13 13:21:03 +0300

    Külgnnevusmaatriksi universaalseks tegemine n-grammidest.

--- website/models.py
+++ website/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-
 # Create your models here.
 from django.utils import timezone
+
+from django.core.validators import MinValueValidator
 
 
 class Content(models.Model):
 	user = models.ForeignKey("auth.User")
 	Tekst = models.TextField(default="")
-	n_gram = models.IntegerField()
+	n_gram = models.PositiveIntegerField(validators=[MinValueValidator(1)], default=1)
 	created = models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
