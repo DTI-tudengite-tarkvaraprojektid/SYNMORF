558b0557a74f3377d2275d924778b65568a0f93c
Marko Kollo <mrxkollo@users.noreply.github.com>
2017-06-21 08:31:41 +0300

    Sisendformi label nimede äravahetamine eesti keelde.

--- website/forms.py
+++ website/forms.py
@@ -3,7 +3,7 @@ from .models import Content
 
 
 class InputForm(forms.ModelForm):
-
 	class Meta:
 		model = Content
 		fields = ("text", "n_gram", "maatriks")
+		labels = {"text": "Tekst" ,"n_gram": 'N-gram pikkus '}
