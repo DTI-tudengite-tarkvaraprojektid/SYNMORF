649619951d8361068a13c1cd3bd79ba37ce5e421
Marko Kollo <mrxkollo@users.noreply.github.com>
2017-06-21 08:36:31 +0300

    Settingute muutmine produktsiooni jaoks

--- synmorf/settings.py
+++ synmorf/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xc!+3cx+&*c633xb*bdk@gepo)z@#!#d!q6egff1g+sdo+un0='
+SECRET_KEY = '**************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '193.40.238.6', 'praktika1.cs.tlu.ee']
 
